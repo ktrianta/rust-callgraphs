9d6abccfff7197f78d62cb491d4f998b15dcdb26
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-12-24 12:12:41 +0100

    Counter getter signatures.

--- new/database-dsl/src/generator.rs
+++ new/database-dsl/src/generator.rs
@@ -204,9 +204,9 @@ fn generate_counters(schema: &ast::DatabaseSchema) -> TokenStream {
     let mut default_impls = TokenStream::new();
     for id in &schema.incremental_ids {
         let ast::IncrementalId {
+            ref name,
             ref typ,
             ref constants,
-            ..
         } = id;
         let field_name = id.get_field_name();
         fields.extend(quote! {
@@ -214,7 +214,7 @@ fn generate_counters(schema: &ast::DatabaseSchema) -> TokenStream {
         });
         let get_fresh_name = id.get_generator_fn_name();
         getter_functions.extend(quote! {
-            fn #get_fresh_name(&mut self) -> #typ {
+            fn #get_fresh_name(&mut self) -> #name {
                 let value = self.#field_name.into();
                 self.#field_name += 1;
                 value
@@ -224,8 +224,8 @@ fn generate_counters(schema: &ast::DatabaseSchema) -> TokenStream {
             let get_constant_name = constant.get_getter_name();
             let value = &constant.value;
             getter_functions.extend(quote! {
-                fn #get_constant_name(&mut self) -> #typ {
-                    #value
+                fn #get_constant_name(&mut self) -> #name {
+                    #value.into()
                 }
             });
         }
